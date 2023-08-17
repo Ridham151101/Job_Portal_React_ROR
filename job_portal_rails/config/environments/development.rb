1ccd469756849bc6247591bbd001f3489a084491
Ridham151101 <ridham.patel@bacancy.com>
2023-08-17 22:03:11 +0530

    created apis for user registration, login and logout with devise, devise-jwt gem

--- job_portal_rails/config/environments/development.rb
+++ job_portal_rails/config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -50,6 +50,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # To render debugging information preserving the response format
+  config.debug_exception_response_format = :api
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
@@ -63,4 +65,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
